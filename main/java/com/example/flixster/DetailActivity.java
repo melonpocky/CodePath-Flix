630f9b671f0313459899aea883044d8682bba986
melonpocky <15367546+melonpocky@users.noreply.github.com>
2020-09-30 17:53:08 -0700

    Add files via upload

--- main/java/com/example/flixster/DetailActivity.java
+++ main/java/com/example/flixster/DetailActivity.java
@@ -24,7 +24,7 @@ import okhttp3.Headers;
 
 public class DetailActivity extends YouTubeBaseActivity {
 
-    private static final String YOUTUBE_API_KEY = "AIzaSyBrLH93i0aCIszfGsVOjQeIo78kcIq0xRs";
+    private static final String YOUTUBE_API_KEY = "";
     public static final String VIDEOS_URL = "https://api.themoviedb.org/3/movie/%d/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
     TextView tvTitle;
     TextView tvOverview;
